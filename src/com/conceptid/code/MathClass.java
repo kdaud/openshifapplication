ad3b8d1a6c45aa84c6b9757f9e7d90ca83253783
kdaud <dkakumirizii@gmail.com>
2020-12-24 10:19:49 +0300

    added hardcoded codes

--- src/com/conceptid/code/MathClass.java
+++ src/com/conceptid/code/MathClass.java
@@ -9,7 +9,15 @@ public class MathClass {
 	
 	public static void callerfunction() {
 		Integer.bitCount(40);
-		int mynumber = Integer.min(-9, -4);
-		System.out.println(mynumber);
+		Integer.min(-9, -4);
+		
+		int [] numbr= {20,45,55,60,70,80};
+		for (int a = 0; a < numbr.length; a++) {
+			
+			if (numbr[a] == 55) {
+				continue;
+			}
+			System.out.println(numbr[a]);
+		}
 	}
 }
