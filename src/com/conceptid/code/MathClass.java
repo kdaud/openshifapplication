c45cfe0659ee0b54afcb2ab16183acfc6f67fbd0
kdaud <dkakumirizii@gmail.com>
2020-12-24 10:28:02 +0300

    more stuff

--- src/com/conceptid/code/MathClass.java
+++ src/com/conceptid/code/MathClass.java
@@ -1,6 +1,5 @@
 package com.conceptid.code;
 
-
 public class MathClass {
 	
 	public static void main(String[] args) {
@@ -14,10 +13,19 @@ public class MathClass {
 		int [] numbr= {20,45,55,60,70,80};
 		for (int a = 0; a < numbr.length; a++) {
 			
-			if (numbr[a] == 55) {
+			if (numbr[a] == 60) {
 				continue;
 			}
 			System.out.println(numbr[a]);
+		}
+	
+	
+		for (int y = 16; y <= 20; y++)
+		{
+			if (y == 14) {
+				break;
+			}
+			System.out.println("Code: " + y);
 		}
 	}
 }
