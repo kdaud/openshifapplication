21da28f967d063d2771b6b7d47f19ca27492f355
kdaud <dkakumirizii@gmail.com>
2020-12-24 09:34:12 +0300

    api added

---
+++ src/com/conceptid/code/MathClass.java
@@ -0,0 +1,15 @@
+package com.conceptid.code;
+
+
+public class MathClass {
+	
+	public static void main(String[] args) {
+		callerfunction();
+	}
+	
+	public static void callerfunction() {
+		Integer.bitCount(40);
+		int mynumber = Integer.min(-9, -4);
+		System.out.println(mynumber);
+	}
+}
